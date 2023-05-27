1d7538771e41603f485f39c95e42cf4bb8811c0f
han <hanxiaolc@gmail.com>
2023-05-27 01:31:50 -0700

    init

---
+++ lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('マサミチ道場'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      )
+    );
+    throw UnimplementedError();
+  }
+
+}
