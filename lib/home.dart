203e8bd0c93613ec3c7438b5a2cea937fab76132
han <hanxiaolc@gmail.com>
2023-06-23 10:16:04 -0700

    init

--- lib/home.dart
+++ lib/home.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _Home();
+  }
+}
+
+class _Home extends State<Home> {
+
+  int _selectedIndex = 0;
+
+  int get selectedIndex => _selectedIndex;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('マサミチ道場'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      )
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book_outlined), label: '单词',),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.edit_outlined), label: '训练',),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outlined), label: '我',),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
-    throw UnimplementedError();
   }
-
 }
